6575ac2e4515cb45150c627a0a67c88a267eaf91
duong91002 <96910467+duong91002@users.noreply.github.com>
2023-04-17 23:29:03 +0700

    done login register

--- src/urls.py
+++ src/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('', home, name='home'),
     path('detail', detail, name='detail'),
     path('login', custom_login, name='login'),
+    path('user/login/', isLogin, name='login'),
+    path('register', custom_register, name='custom_register'),
     path('search', search, name='search'),
     path('api/category/', getCategory, name='get-category'),
-
+    path('user/add/', isResister, name="addUser")
 ]
--- src/views.py
+++ src/views.py
@@ -1,7 +1,15 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.contrib.auth.models import *
 from .models import *
 from django.http import JsonResponse
+import logging
+import traceback
+from django.contrib.auth.hashers import make_password
+from django.http import JsonResponse
+import json
+from django.contrib.auth.hashers import check_password
 # Create your views here.
 
 
@@ -10,8 +18,69 @@ def home(request):
     return render(request, 'home.html', {'user': user})
 
 
+def isLogin(request):
+    try:
+        if request.method == "POST":
+            raw_data = request.body
+            data_string = raw_data.decode('utf-8')
+            data = json.loads(data_string)
+
+            username = data['username']
+            password = data['password']
+            isLogin = False
+
+            users = User.objects.all()
+            for item in users:
+                if item.username == username and check_password(password, item.password):
+                    isLogin = True
+                    user = authenticate(
+                        request, username=username, password=password)
+                    login(request, user)
+        return JsonResponse({"isLogin": isLogin})
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
+
+def isResister(request):
+    try:
+        if request.method == "POST":
+            raw_data = request.body
+            data_string = raw_data.decode('utf-8')
+            data = json.loads(data_string)
+
+            username = data['username']
+            password = data['password']
+            hashed_password = make_password(password)
+            email = data['email']
+
+            users = User.objects.all()
+            isUse = False
+            for item in users:
+                if item.username == username:
+                    isUse = True
+
+            if isUse == False:
+                user = User.objects.create(
+                    username=username, password=hashed_password, email=email, is_staff=True)
+                group = Group.objects.get(name='User')
+                user.groups.add(group)
+                user = authenticate(
+                    request, username=username, password=password)
+                login(request, user)
+        return JsonResponse({"isUse": isUse})
+
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
+
 def custom_login(request):
-    return render(request, 'admin/custom_login.html')
+    users = User.objects.all()
+    return render(request, 'admin/custom_login.html', {"user": users})
+
+
+def custom_register(request):
+    users = User.objects.all()
+    return render(request, 'admin/custom_register.html', {"user": users})
 
 
 def search(request):
@@ -21,10 +90,6 @@ def search(request):
 
 def detail(request):
     return render(request, 'detail.html')
-
-
-def login(request):
-    return render(request, 'login.html')
 
 
 def getCategory(request):
